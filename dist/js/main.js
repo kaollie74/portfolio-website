23ffa10b4d62095dcc70d11ddb9c0f17ff501261
kaollie74 <kaollie74@gmail.com>
2019-10-26 12:52:13 -0500

    fixed branding side 0f page to slide up when menu is clicked

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,12 +22,14 @@ function toggleMenu() {
     $('.menu-btn').addClass('close');
     $('.menu').addClass('show');
     $('.menu-nav').addClass('show');
+    $('.menu-branding').addClass('show')
     navItems.forEach(item => item.addClass('show'));
     showMenu = true;
   } else if (showMenu) {
     $('.menu-btn').removeClass('close');
     $('.menu').removeClass('show');
     $('.menu-nav').removeClass('show');
+    $('.menu-branding').removeClass('show');
     navItems.forEach(item => item.removeClass('show'));
     // set Menu State
     showMenu = false;
